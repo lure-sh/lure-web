f2273b9576271ba0f91873ddb9e79a16ebbf662d
Arsen Musayelyan <moussaelianarsen@gmail.com>
2022-12-20 14:22:54 -0800

    Add docker build script

---
+++ docker/docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker buildx build --platform linux/amd64 --tag arsen6331/lure-web:amd64 .
+docker buildx build --platform linux/arm64/v8 --tag arsen6331/lure-web:arm64 .
+
+docker login
+docker push arsen6331/lure-web -a
+
+docker manifest rm arsen6331/lure-web:latest
+docker manifest create arsen6331/lure-web:latest --amend arsen6331/lure-web:arm64 --amend arsen6331/lure-web:amd64
+docker manifest push arsen6331/lure-web:latest
