7480e8a615b448be26cf91361a8a702651f8b171
Arsen Musayelyan <moussaelianarsen@gmail.com>
2022-12-21 11:36:39 -0800

    Disable docker build cache

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64 --tag arsen6331/lure-web:amd64 .
-docker buildx build --platform linux/arm64/v8 --tag arsen6331/lure-web:arm64 .
+docker buildx build --platform linux/amd64 --tag arsen6331/lure-web:amd64 --no-cache .
+docker buildx build --platform linux/arm64/v8 --tag arsen6331/lure-web:arm64 --no-cache .
 
 docker login
 docker push arsen6331/lure-web -a
